55add945497a319758e0daa29472ef2e18096474
Shubham Chaudhari <61900242+shubhamchau222@users.noreply.github.com>
2021-03-18 16:50:38 +0530

    updated train_and_validation.py

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -15,6 +15,11 @@ import joblib
 import json
 
 
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
 
 def train_and_evaluate(config_path):
     config = read_params(config_path)
@@ -25,17 +30,56 @@ def train_and_evaluate(config_path):
 
     alpha = config["estimators"]["ElasticNet"]["params"]["alpha"]
     l1_ratio = config["estimators"]["ElasticNet"]["params"]["l1_ratio"]
-
     target = [config["base"]["target_col"]]
 
     train = pd.read_csv(train_data_path, sep=",")
     test = pd.read_csv(test_data_path, sep=",")
 
+    train.drop(['Unnamed: 0'] , inplace = True , axis = 1)
+    test.drop(['Unnamed: 0'] , inplace = True , axis = 1)
+
     train_y = train[target]
     test_y = test[target]
 
-    train_x = train.drop(target , axis= 1)
-    test_x  = test.drop(target , axis= 1)
+    train_x = train.drop( target, axis= 1)
+    test_x  = test.drop( target , axis= 1)
+
+    lr = ElasticNet(
+            alpha=alpha, 
+            l1_ratio=l1_ratio, 
+            random_state=random_state)
+    lr.fit(train_x, train_y)
+
+    predicted_qualities = lr.predict(test_x)
+
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+    print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+    print("  RMSE: %s" % rmse)
+    print("  MAE: %s" % mae)
+    print("  R2: %s" % r2)
+
+    os.makedirs(model_dir, exist_ok=True)
+    model_path = os.path.join(model_dir, "model.joblib")
+
+    joblib.dump(lr, model_path)
 
 
+
+  
+
+if __name__=="__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_args = args.parse_args()
+    train_and_evaluate(config_path=parsed_args.config)
+
+
+
+
+if __name__=="__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_args = args.parse_args()
+    train_and_evaluate(config_path=parsed_args.config)
 
