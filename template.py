4c92a7087a881de77e8f72183026d5101d8a5fcd
Shubham Chaudhari <61900242+shubhamchau222@users.noreply.github.com>
2021-03-18 06:26:13 +0530

    second commit

--- template.py
+++ template.py
@@ -18,8 +18,8 @@ for dir_ in dirs:
         pass 
 
 files = [
-   " dvc.yml" ,
-    "params.yml" ,
+   " dvc.yaml" ,
+    "params.yaml" ,
     ".gitignore" ,
     os.path.join("src" , "__init__.py")
 ]
