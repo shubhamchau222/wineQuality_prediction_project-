3ea31a9dfab35d9cb628d0ac34fd77486817bbdf
Shubham Chaudhari <61900242+shubhamchau222@users.noreply.github.com>
2021-03-18 16:52:08 +0530

    updated train_evaluation file

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -74,12 +74,3 @@ if __name__=="__main__":
     parsed_args = args.parse_args()
     train_and_evaluate(config_path=parsed_args.config)
 
-
-
-
-if __name__=="__main__":
-    args = argparse.ArgumentParser()
-    args.add_argument("--config", default="params.yaml")
-    parsed_args = args.parse_args()
-    train_and_evaluate(config_path=parsed_args.config)
-
